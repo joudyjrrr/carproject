66a5053ac6403866a108ddbe5a1d9a439079ac42
joudyjrrr <joudyjrrr@gmail.com>
2023-12-04 18:11:11 +0300

    first commit

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Home() {
+    return (
+      <h1>Not Found</h1>
+    )
+  }
+  
---
+++ components/index.ts
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomeButton";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer ";
+import CarCard from "./carCard"
+import CarDetails from "./CarDetails"
+import ShowMore from "./ShowMore"
+export {
+    Hero,
+    CustomButton,
+    NavBar,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    Footer,
+    CarCard,
+    CarDetails,
+    ShowMore
+}
